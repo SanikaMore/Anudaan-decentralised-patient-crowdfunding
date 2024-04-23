2a008356a88322ac1c7ff9bb5b751fa7088a9591
unknown <krishnan.subramanian@spit.ac.in>
2024-04-23 20:10:10 +0530

    with backend

--- src/pages/CampaignDetails.jsx
+++ src/pages/CampaignDetails.jsx
@@ -145,7 +145,7 @@ const CampaignDetails = () => {
             <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Story</h4>
 
             <div className="mt-[20px]">
-              <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">{state.description}</p>
+            <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">{state.description}</p>
             </div>
           </div>
 
